6178d47595942de63d579c9abe51a3246f616e84
smileexx <smilee900@gmail.com>
2021-10-21 17:47:13 +0300

    parse update

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 const API_URL = "https://api.telegram.org/bot"
@@ -22,34 +23,56 @@ func main() {
 	})
 
 	// Hnadle Bot Webhook updates
-	http.HandleFunc("/"+os.Getenv("BOT_TOKEN"), func(w http.ResponseWriter, req *http.Request) {
-		w.WriteHeader(200)
-		body, _ := ioutil.ReadAll(req.Body)
-		fmt.Printf("%s", body)
-	})
+	http.HandleFunc("/"+os.Getenv("BOT_TOKEN"), handleUpdate)
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 
-	// offset := 0
+	// updateLoop()
+}
 
-	// for {
-	// 	currentTime := time.Now()
-	// 	log.Println(currentTime.Format("2006-01-02 15:04:05.000000"))
-	// 	updates, err := getUpdates(offset)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	for _, update := range updates {
-	// 		offset = update.UpdateId + 1
-	// 		log.Println(update.Message)
-	// 		err = respond(update.Message)
-	// 	}
-	// 	time.Sleep(time.Second)
-	// }
+func updateLoop() {
 
+	offset := 0
+
+	for {
+		currentTime := time.Now()
+		log.Println(currentTime.Format("2006-01-02 15:04:05.000000"))
+		updates, err := getUpdates(offset)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, update := range updates {
+			offset = update.UpdateId + 1
+			log.Println(update.Message)
+			err = respond(update.Message)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		time.Sleep(time.Second)
+	}
 }
 
 func buildUrl(param string) string {
 	return API_URL + os.Getenv("BOT_TOKEN") + param
+}
+
+func handleUpdate(w http.ResponseWriter, req *http.Request) {
+
+	w.WriteHeader(200)
+	body, _ := ioutil.ReadAll(req.Body)
+	fmt.Printf("%s", body)
+	var update Update
+	err := json.Unmarshal(body, &update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(update)
+	err = respond(update.Message)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func getUpdates(offset int) ([]Update, error) {
